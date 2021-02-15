27822b804b5d880f35c27535ea360866e25fd3be
MaTu-V <1261816122@qq.com>
2021-02-15 21:11:16 +0800

    项目结构初始化

---
+++ mall-coupon/src/test/java/com/dun/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dun.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
